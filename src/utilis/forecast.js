6e6d06b5d937c821fabd4d8a79ea046ee89aa482
EslamTeleb1 <et1136@fayoum.edu.eg>
2020-10-08 17:18:19 +0200

    Change tempreature form fahrenheit to celsius

--- src/utilis/forecast.js
+++ src/utilis/forecast.js
@@ -3,7 +3,7 @@ const request=require('request');
 const forecast=(lat,long,callBack)=>{
    // const url = "http://api.weatherstack.com/current?access_key=729fff8f0fcc8e19e766f1ea02353d5a&query=New%20York"
     const key='729fff8f0fcc8e19e766f1ea02353d5a';
-    const url = `http://api.weatherstack.com/current?access_key=${key}&query=${lat},${long}&units=f`;
+    const url = `http://api.weatherstack.com/current?access_key=${key}&query=${lat},${long}`;
 
 request({url ,json:true}, (error, {body}) => {
     //console.log(error,response);
